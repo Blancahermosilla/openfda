117826413af063be6dbcb596514e8ddb1fb757f9
b.hermosilla.2017 <blancahermosillacampos@gmail.com>
2018-03-20 15:59:08 +0100

    practice-1

---
+++ practice-github/show_repos.py
@@ -0,0 +1,22 @@
+import http.client
+import json
+
+headers = {'User-Agent': 'http-client'}
+
+conn = http.client.HTTPSConnection("api.github.com") #send a request
+conn.request("GET", "/users/Blancahermosilla/repos", None, headers)
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+repos_raw = r1.read().decode("utf-8")
+conn.close()
+
+repos = json.loads(repos_raw)
+#print("total number of repos", len(repos))
+print(repos)
+
+repo = repos[0]
+#print(repo)
+for i in repo:
+	repos = repo[i]
+print(repo["full_name"])
+print("The owner of the first repository is", repo['owner']['login'])
