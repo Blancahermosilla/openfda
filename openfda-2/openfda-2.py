8e4c766610d5c706677f77b91938da076d071cbf
b.hermosilla.2017 <blancahermosillacampos@gmail.com>
2018-04-03 16:46:19 +0200

    rename

--- openfda-2/openfda-2.py
+++ openfda-2/openfda-2.py
@@ -4,7 +4,7 @@ import json
 headers = {'User-Agent': 'http-client'}
 
 conn = http.client.HTTPSConnection("api.fda.gov")
-conn.request("GET", "/drug/label.json?search=active_ingredient:acetylsalicylic&limit=4", None, headers) #4 is the total number of records matching the search
+conn.request("GET", "/drug/label.json?search=active_ingredient:acetylsalicylic&limit=10", None, headers) #4 is the total number of records matching the search
 r1 = conn.getresponse()
 print(r1.status, r1.reason)
 repos_raw = r1.read().decode("utf-8")
@@ -12,7 +12,7 @@ conn.close()
 
 repos = json.loads(repos_raw)
 
-##
+#of the 4 total matches two of them are empty dicts
 for i in range(len(repos['results'])):
     if repos['results'][i]['openfda'] == {}:
         continue
